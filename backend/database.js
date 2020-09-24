a6362d2ef68f7face12b5cf1215f030a73f4aa20
Nikolaj Hoggins <mollevip@gmail.com>
2020-09-24 09:38:18 +0200

    All tables set up

--- backend/database.js
+++ backend/database.js
@@ -14,8 +14,38 @@ con.connect(function (err) {
   console.log("Connected!");
 });
 
+//Create admin table;
 con.query(
-  "CREATE TABLE admins(id int AUTO_INCREMENT PRIMARY KEY, email varchar(255) NOT NULL, password varchar(255) NOT NULL);",
+  "CREATE TABLE IF NOT EXISTS admins(id int AUTO_INCREMENT PRIMARY KEY, email varchar(255) NOT NULL, password varchar(255) NOT NULL);",
+  (err) => {
+    if (err) console.log("ERROR: \n", err);
+  }
+);
+
+//Create customers table;
+con.query(
+  "CREATE TABLE IF NOT EXISTS customers(id int AUTO_INCREMENT PRIMARY KEY, name varchar(255), email varchar(255) NOT NULL, password varchar(255) NOT NULL, address varchar(255), phone_number varchar(255));",
+  (err) => {
+    if (err) console.log("ERROR: \n", err);
+  }
+);
+
+con.query(
+  "CREATE TABLE IF NOT EXISTS products(id int AUTO_INCREMENT PRIMARY KEY,name varchar(255),description varchar(255),price double,picture varchar(255));",
+  (err) => {
+    if (err) console.log("ERROR: \n", err);
+  }
+);
+
+con.query(
+  "CREATE TABLE IF NOT EXISTS orders(id int AUTO_INCREMENT PRIMARY KEY,date varchar(255),customerid int,FOREIGN KEY (customerid) REFERENCES customers(id));",
+  (err) => {
+    if (err) console.log("ERROR: \n", err);
+  }
+);
+
+con.query(
+  "CREATE TABLE orderdetails(id int AUTO_INCREMENT PRIMARY KEY,orderid int,productid int,quantity int,FOREIGN KEY (orderid) REFERENCES customers(id),FOREIGN KEY (productid) REFERENCES products(id));",
   (err) => {
     if (err) console.log("ERROR: \n", err);
   }
