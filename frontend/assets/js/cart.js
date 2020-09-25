67f7f97def411f4e4f738eb55f93505dc3248e8a
Nikolaj Hoggins <mollevip@gmail.com>
2020-09-25 09:34:32 +0200

    Cart shows items

--- frontend/assets/js/cart.js
+++ frontend/assets/js/cart.js
@@ -6,17 +6,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 function main() {
   const cart = JSON.parse(localStorage.getItem("cart"));
+  if (!cart) {
+    document.getElementById("cart").innerHTML = "No items in cart";
+    return;
+  }
   cart.forEach((element) => {
     backendService.getProductById(element, ({ result }) => {
       console.log(result);
       var newItem = document.createElement("div");
       var cartButton = document.createElement("button");
 
-      cartButton.innerHTML = "Add to cart";
-      cartButton.onclick = () => addToCart(result.id);
-
+      cartButton.innerHTML = "Remove from cart";
+      cartButton.onclick = () => removeItem(result.id);
+      newItem.className = "test";
       newItem.innerHTML = `<h3>${result.name}</h3>`;
-      newItem.innerHTML += `<img src='https://freesvg.org/img/Placeholder.png'></img> <br>`;
       newItem.innerHTML += `<p>${result.description}</p> <br>`;
       newItem.innerHTML += `<p>kr. ${result.price.toFixed(2)}</p>`;
 
