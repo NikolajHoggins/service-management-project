3c1556b3dc9b3ef4b48317941422bff2603d6a87
Nikolaj Hoggins <mollevip@gmail.com>
2020-09-25 08:26:02 +0200

    Frontend login

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -3,11 +3,39 @@ const backendService = new BackendService("jesper");
 const cartService = new CartService();
 
 backendService.getProducts().then((resp) => getList(resp));
-function logIn(e) {
-  e.preventDefault();
-  var username = document.getElementById("inputField1").value;
-  var password = document.getElementById("inputField2").value;
 
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("loginForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    document.getElementById("errorMessage").innerText = "";
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    backendService.login(username, password, (res) => {
+      if (res.status === "success") {
+        //Set token
+      } else {
+        //Error msg
+        document.getElementById("errorMessage").innerText =
+          "ERROR, WRONG USERNAME OR PASSWORD";
+      }
+    });
+
+    //Attempt login
+
+    if (status == "user") {
+      goToShop();
+    } else if (status == "admin") {
+      goToShop();
+    } else {
+    }
+  });
+});
+
+function logIn() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  console.log(username, password);
   if (username == "user" && password == "password") {
     status = "user";
   } else if (username == "admin" && password == "password") {
@@ -25,8 +53,7 @@ function logIn(e) {
 }
 
 function getList(resp) {
-
-  resp.forEach(element => {
+  resp.forEach((element) => {
     console.log(element.name);
     var container = document.getElementById("flex-container");
     var newItem = document.createElement("div");
